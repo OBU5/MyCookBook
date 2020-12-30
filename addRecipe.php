c98df35848f27165bec9e8f9807027cfd0a6b91c
Ondřej Bureš <30869245+OBU5@users.noreply.github.com>
2020-12-30 10:36:16 +0100

    all recipe data are now stored into database

--- addRecipe.php
+++ addRecipe.php
@@ -1,6 +1,7 @@
 <?php
 $connect = mysqli_connect("localhost", "root", "", "test");
 $connect->set_charset("UTF-8");
+$error = false;
 session_start();
 // user must be logged in
 if (!isset($_SESSION["username"])) {
@@ -11,113 +12,161 @@ if (!isset($_SESSION["username"])) {
         $recipeAuthor = $_SESSION["username"];
 
         //get recipename
-        if (empty($_POST["recipename"])) {
-            echo '<script>alert("Není vyplněno jméno receptu")</script>';
-        } else {
-            $recipename = mysqli_real_escape_string($connect, $_POST["recipename"]);
+        if (!$error) {
+            if (empty($_POST["recipename"])) {
+                echo '<script>alert("Není vyplněno jméno receptu")</script>';
+                $error = true;
+            } else {
+                $recipename = mysqli_real_escape_string($connect, $_POST["recipename"]);
+            }
         }
 
         //get directions
-        if (empty($_POST["directions"])) {
-            echo '<script>alert("Není vyplněn postup")</script>';
-        } else {
-            $directions = mysqli_real_escape_string($connect, $_POST["directions"]);
+        if (!$error) {
+
+            if (empty($_POST["directions"])) {
+                echo '<script>alert("Není vyplněn postup")</script>';
+                $error = true;
+            } else {
+                $directions = mysqli_real_escape_string($connect, $_POST["directions"]);
+            }
         }
 
         // get user_id of signed user
-        $query = "SELECT ID FROM Users WHERE username = '$recipeAuthor'";
-        $result = $connect->query($query);
-        if (mysqli_num_rows($result) <= 0) {
-            echo '<script>alert("you are not signed in!")</script>';
-        } else {
-            $row = $result->fetch_assoc();
-            $recipeAuthor_id = $row['ID'];
+        if (!$error) {
+            $query = "SELECT ID FROM Users WHERE username = '$recipeAuthor'";
+            $result = $connect->query($query);
+            if (mysqli_num_rows($result) <= 0) {
+                echo '<script>alert("you are not signed in!")</script>';
+                $error = true;
+            } else {
+                $row = $result->fetch_assoc();
+                $recipeAuthor_id = $row['ID'];
+            }
         }
 
 
         // get originCountry_id of selected origin country 
-        if (empty($_POST["originCountry"])) {
-            echo '<script>alert("Není zvolena země původu")</script>';
-        } else {
-            $originCountry = mysqli_real_escape_string($connect, $_POST["originCountry"]);
+        if (!$error) {
 
-            $query = "SELECT ID FROM OriginCountry WHERE name = '$originCountry'";
-            $result = $connect->query($query);
-            if (mysqli_num_rows($result) > 0) {
-                $row = $result->fetch_assoc();
-                $originCountry_id = $row['ID'];
+            if (empty($_POST["originCountry"])) {
+                echo '<script>alert("Není zvolena země původu")</script>';
+                $error = true;
+            } else {
+                $originCountry = mysqli_real_escape_string($connect, $_POST["originCountry"]);
+
+                $query = "SELECT ID FROM OriginCountry WHERE name = '$originCountry'";
+                $result = $connect->query($query);
+                if (mysqli_num_rows($result) > 0) {
+                    $row = $result->fetch_assoc();
+                    $originCountry_id = $row['ID'];
+                }
             }
         }
 
         // store image on server and get created URL if present
-        $imgUrl = "";
-        if (isset($_FILES['img']) && ($_FILES['img']['size'] > 0)) {
-            $uploaddir = 'Uploads/';
-            $uploadfile = basename($_FILES['img']['name']);
-            $uploadfile = str_replace(' ', '-', $uploadfile);
-            // check if the image doesn't exists... if it does, change name in order to keep existing one
-            $i = '';
-            while (file_exists($uploaddir . $i . $uploadfile)) {
-                $i++;
-            }
-            $uploadfile = $uploaddir . $i . $uploadfile;
-            echo $uploadfile;
-            if (move_uploaded_file($_FILES["img"]["tmp_name"], $uploadfile)) {
-                $imgUrl = 'http://' . $_SERVER['SERVER_NAME'] . '/MyCookBook/' . $uploadfile;
-            } else {
-                echo '<script>alert("byl zvolen soubor v chybném formátu")</script>';
-            }
-            /*
+        if (!$error) {
+            $imgUrl = "";
+            if (isset($_FILES['img']) && ($_FILES['img']['size'] > 0)) {
+                $uploaddir = 'Uploads/';
+                $uploadfile = basename($_FILES['img']['name']);
+                $uploadfile = str_replace(' ', '-', $uploadfile);
+                // check if the image doesn't exists... if it does, change name in order to keep existing one
+                $i = '';
+                while (file_exists($uploaddir . $i . $uploadfile)) {
+                    $i++;
+                }
+                $uploadfile = $uploaddir . $i . $uploadfile;
+                //echo $uploadfile;
+                if (move_uploaded_file($_FILES["img"]["tmp_name"], $uploadfile)) {
+                    $imgUrl = 'http://' . $_SERVER['SERVER_NAME'] . '/MyCookBook/' . $uploadfile;
+                } else {
+                    echo '<script>alert("byl zvolen soubor v chybném formátu")</script>';
+                }
+                /*
             echo '<pre>';
             echo 'Here is some more debugging info:';
             print_r($_FILES);
             print "</pre>";*/
-        } else {
-            echo '<script>alert("Není vložen obrázek")</script>';
+            } else {
+                echo '<script>alert("Není vložen obrázek")</script>';
+                $error = true;
+            }
         }
 
 
         // store Recipe into database
-        $query = "INSERT INTO Recipes(author_id, name, date, directions, originCountry_id, imgUrl) VALUES('$recipeAuthor_id', '$recipename', curdate(), '$directions', '$originCountry_id', '$imgUrl')";
-        if (mysqli_query($connect, $query)) {
-            echo '<script>alert("recept byl úspěšně přidán")</script>';
-            // remember id of stored recipe (it will be handy for "Recipe_Ingredient" table)
-            $currentRecipeID = $connect->insert_id;
-        } else {
-            echo '<script>alert("Recept nebyl přidán, došlo k neočekávané chybě")</script>';
+        if (!$error) {
+
+            $query = "INSERT INTO Recipes(author_id, name, date, directions, originCountry_id, imgUrl) VALUES('$recipeAuthor_id', '$recipename', curdate(), '$directions', '$originCountry_id', '$imgUrl')";
+            if (mysqli_query($connect, $query)) {
+                echo '<script>alert("recept byl úspěšně přidán")</script>';
+                // remember id of stored recipe (it will be handy for "Recipe_Ingredient" table)
+                $currentRecipeID = $connect->insert_id;
+            } else {
+                echo '<script>alert("Recept nebyl přidán, došlo k neočekávané chybě")</script>';
+                $error = true;
+            }
         }
 
-        //store ingredients into database
-        $ingredients = $_POST['ingredients'];
-        $quantities = $_POST['quantities'];
-        $units = $_POST['units'];
-        $ingredients_ID = array();
+        // store relationship between MealCategory and stored recipe into database
 
-        // check, if ingredients, quantities and units are really arrays
-        if ((is_array($ingredients) || is_object($ingredients)) && (is_array($quantities) || is_object($quantities)) && (is_array($units) || is_object($units))) {
-            // store each ingredient and relationship between ingredient and recipe
-            for ($i = 0; $i < sizeof($ingredients); $i++) {
-                if (!empty($ingredients[$i])) {
-                    // store ingredient
-                    $query = "INSERT INTO Ingredients(name, quantity, unit) VALUES('$ingredients[$i]', '$quantities[$i]', '$units[$i]')";
-                    if (mysqli_query($connect, $query)) {
-                        // remember id of stored ingredient (it will be handy for "Recipe_Ingredient" table)
-                        $ingredients_ID[$i] = $connect->insert_id;
-                    } else {
-                        echo '<script>alert("Došlo k neočekávané chybě při pokusu o uložení ingredience ")</script>';
-                    }
 
-                    // store relationship between ingredients and stored recipe into database
+        //get "mealCategory" elements  
+        if (!$error) {
 
-                    $query = "INSERT INTO Recipe_Ingredients(recipe_id, ingredient_id) VALUES('$currentRecipeID', '$ingredients_ID[$i]')";
+            $query = "SELECT ID FROM MealCategory";
+            $result = $connect->query($query);
+            $i = 1;
+            while ($i <= $result->num_rows) {
+                // output data of each row
+                if (isset($_POST["mealCategory" . $i])) {
+                    //echo $i;
+                    $query = "INSERT INTO Recipe_MealCategory(recipe_id, mealCategory_id) VALUES('$currentRecipeID', '$i')";
                     if (mysqli_query($connect, $query)) {
                     } else {
                         echo '<script>alert("Došlo k neočekávané chybě při pokusu o uložení vztahu mezi ingrediencí a receptem ")</script>';
+                        $error = true;
                     }
                 }
+                $i++;
             }
-        } else {
-            echo '<script>alert("Došlo k neočekávané chybě")</script>';
+        }
+
+        if (!$error) {
+
+            //store ingredients into database
+            $ingredients = $_POST['ingredients'];
+            $quantities = $_POST['quantities'];
+            $units = $_POST['units'];
+            $ingredients_ID = array();
+
+            // check, if ingredients, quantities and units are really arrays
+            if ((is_array($ingredients) || is_object($ingredients)) && (is_array($quantities) || is_object($quantities)) && (is_array($units) || is_object($units))) {
+                // store each ingredient and relationship between ingredient and recipe
+                for ($i = 0; $i < sizeof($ingredients); $i++) {
+                    if (!empty($ingredients[$i])) {
+                        // store ingredient
+                        $query = "INSERT INTO Ingredients(name, quantity, unit) VALUES('$ingredients[$i]', '$quantities[$i]', '$units[$i]')";
+                        if (mysqli_query($connect, $query)) {
+                            // remember id of stored ingredient (it will be handy for "Recipe_Ingredient" table)
+                            $ingredients_ID[$i] = $connect->insert_id;
+                        } else {
+                            echo '<script>alert("Došlo k neočekávané chybě při pokusu o uložení ingredience ")</script>';
+                        }
+
+                        // store relationship between ingredients and stored recipe into database
+
+                        $query = "INSERT INTO Recipe_Ingredients(recipe_id, ingredient_id) VALUES('$currentRecipeID', '$ingredients_ID[$i]')";
+                        if (mysqli_query($connect, $query)) {
+                        } else {
+                            echo '<script>alert("Došlo k neočekávané chybě při pokusu o uložení vztahu mezi ingrediencí a receptem ")</script>';
+                        }
+                    }
+                }
+            } else {
+                echo '<script>alert("Došlo k neočekávané chybě")</script>';
+            }
         }
     }
 }
@@ -252,14 +301,14 @@ if (!isset($_SESSION["username"])) {
             <div name="mealCategoryDiv" class="mealCategoryDiv">
 
                 <?php
-                $query = "SELECT name FROM MealCategory";
+                $query = "SELECT ID, name FROM MealCategory";
                 $result = $connect->query($query);
                 if ($result->num_rows > 0) {
                     // output data of each row
                     $i = 0;
                     while ($row = $result->fetch_assoc()) {
                         $checked = isset($_POST["mealCategory" . $i]) ? "checked " : "";
-                        echo '<input type="checkbox" name="mealCategory' . $i . '"' . $checked . ' >' . $row['name'] . '<br>';
+                        echo '<input type="checkbox" name="mealCategory' . $i . '"' . $checked . ' value =' . $row['ID'] . '>' . $row['name'] . '<br>';
                         $i++;
                     }
                 }
